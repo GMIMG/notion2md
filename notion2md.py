ab2a8f10854c2954364e4098d7dbf5c5b0683ece
Jimin Hwang <gmimg9@gmail.com>
2020-08-09 03:38:06 +0900

    v0.2

--- notion2md.py
+++ notion2md.py
@@ -35,12 +35,15 @@ class Table:
 	def parse_csv(file, delimiter=',', quotechar='"'):
 		return Table(list(csv.reader(file, delimiter=delimiter, quotechar=quotechar)))
 
+
 # https://github.com/dreamgonfly/TIL
 def main():
 	try:
 		os.mkdir('./CONVERTED')
 	except:
-		pass
+		shutil.rmtree('./CONVERTED')
+		os.mkdir('./CONVERTED')
+
 	EXPORT_ZIP_DIR = Path('./')
 	MD_REPO = Path('./CONVERTED')
 
@@ -53,62 +56,78 @@ def main():
 		directory_to_extract_to = Path(EXPORT_ZIP_DIR, path_to_zip_file.stem)
 
 		# unzip
-		with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
-			zip_ref.extractall(directory_to_extract_to)
+		with zipfile.ZipFile(path_to_zip_file, 'r') as zf:
+			zipInfo = zf.infolist()
+			for member in zipInfo:
+				try:
+					# print(member.filename.encode('cp437').decode('euc-kr', 'ignore'))
+					# member.filename = member.filename.encode('cp437').decode('euc-kr', 'ignore')
+					zf.extract(member, directory_to_extract_to)
+				except:
+					print('Failed : ', member)
+			# zip_ref.extractall(directory_to_extract_to)
 
-		# markdown만 가져오기
-		glob_result = list(directory_to_extract_to.glob('*.md'))
-		# 가져온게 1개가 아니면 에러(assert)
-		assert len(glob_result) == 1
-		# 첫번째 파일
-		markdown_to_edit = glob_result[0]
-		# 을 제목으로 하기
-		subject_name = '-'.join(markdown_to_edit.stem.split('-'))
+		print('-------------------------------------------------')
 
-		assets_folder = Path(directory_to_extract_to, markdown_to_edit.stem)
-		args_csv = {x[-36:-4]: x for x in os.listdir(assets_folder)
-				if x.endswith('.csv')}
+		for (root,dirs,files) in os.walk(directory_to_extract_to):
+			for f in files:
+				if not os.path.exists(Path(root,f)):
+					continue
+				if not re.match('^(\d\d\d\d-\d\d-\d\d-).+(.md)', f):
+					continue
 
-		# 파일 제목에 확장자 붙여서
-		new_name = subject_name + '.md'
-		# 내 git 폴더에 복사
-		new_markdown = MD_REPO.joinpath(new_name)
+				print(f)
 
-		# 이미지 옮기기 (원래 md파일 이름과 같은 폴더안에 있음)
-		with markdown_to_edit.open(encoding='UTF8') as to_edit, new_markdown.open('w', encoding='UTF8') as editted:
-			img_num = 0
-			# markdown 파일 라인별로 읽어서
-			for line in to_edit:
-				# 이미지 경로 찾음
-				match = re.match('\!?\[.+?\]\((.+?)\)', line)
-				if match:
-					# img
-					if line[0] == '!':
-						image_name = unquote(match.group(1))
-						file_name = os.path.basename(image_name)
-						file_name = os.path.splitext(file_name)[0]
-						image_path = directory_to_extract_to.joinpath(image_name)
-						# github에서는 images 라는 폴더에 넣어서 관리
-						# new_image_path = MD_REPO.joinpath('assets/img', image_name)
-						new_image_path = MD_REPO.joinpath(image_name)
-						new_image_path.parent.mkdir(parents=True, exist_ok=True)
-						# image_path.rename(new_image_path)
-						shutil.move(image_path, new_image_path)
-						# new_line = f"![image{img_num}_{file_name}](assets/img/{image_name})"
-						new_line = f"![image{img_num}_{file_name}]({image_name})"
-						img_num += 1
-						editted.write(new_line)
-					# csv
-					elif line[-6:-1] == '.csv)':
-						with Path(assets_folder, args_csv[line[-38:-6]]).open('r', encoding='utf-8') as csv:
-							table = Table.parse_csv(csv, ',', '"')
-							editted.write(table.markdown())
-					else:
-						editted.write(line)
-				else:
-					editted.write(line)
-				
+				markdown_to_edit = Path(root, f)
+				subject_name = ' '.join(markdown_to_edit.stem.split()[:-1])
+
+				assets_folder = Path(root, markdown_to_edit.stem)
+				if os.path.exists(assets_folder):
+					args_csv = {x[-36:-4]: x for x in os.listdir(assets_folder)
+							if x.endswith('.csv')}
+
+
+				# 파일 제목에 확장자 붙여서
+				new_name = subject_name + '.md'
+				# 내 git 폴더에 복사
+				new_markdown = Path(MD_REPO, new_name)
+
+				# 이미지 옮기기 (원래 md파일 이름과 같은 폴더안에 있음)
+				with markdown_to_edit.open(encoding='UTF8') as to_edit, new_markdown.open('w', encoding='UTF8') as editted:
+					img_num = 0
+					# markdown 파일 라인별로 읽어서
+					for line in to_edit:
+						# 이미지 경로 찾음
+						match = re.match('\!?\[.+?\]\((.+?)\)', line)
+						if match:
+							# img
+							if line[0] == '!':
+								path_image = unquote(match.group(1))
+								image_name_noext = Path(path_image).stem
+								image_name = Path(path_image).name
+
+								image_path = Path(root, path_image)
+
+								new_image_path = Path(MD_REPO, subject_name, image_name)
+								new_image_path.parent.mkdir(parents=True, exist_ok=True)
+								
+								shutil.move(image_path, new_image_path)
+								
+								new_line = f"![image{img_num}_{image_name_noext}]({subject_name}/{image_name})"
+								img_num += 1
+								editted.write(new_line)
+							# csv
+							elif line[-6:-1] == '.csv)':
+								with Path(assets_folder, args_csv[line[-38:-6]]).open('r', encoding='utf-8') as csv:
+									table = Table.parse_csv(csv, ',', '"')
+									editted.write(table.markdown())
+							else:
+								editted.write(line)
+						else:
+							editted.write(line)
+				print('DONE', markdown_to_edit)			
 		shutil.rmtree(path_to_zip_file.stem)
+		
 
 if __name__ == '__main__':
 	main()
